0a0e754f97350536b7a41d8c12d4bb6230cc68c5
Misha Bruml <mishabruml@gmail.com>
2018-08-30 19:01:54 +0100

    counter app lives elsewhere now, modified index page

--- counterapp/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDom from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import App from "./App.js";
-
-ReactDom.render(<App />, document.getElementById("root"));
